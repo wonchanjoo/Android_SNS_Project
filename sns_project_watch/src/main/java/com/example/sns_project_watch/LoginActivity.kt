b0bf3be6bdb532200a607dc6e58155917b06003d
chanjoo <wcj0911@naver.com>
2022-11-26 17:13:28 +0900

    워치 로그인 기능 추가

--- sns_project_watch/src/main/java/com/example/sns_project_watch/LoginActivity.kt
+++ sns_project_watch/src/main/java/com/example/sns_project_watch/LoginActivity.kt
@@ -3,8 +3,8 @@ package com.example.sns_project_watch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sns_project_watch.databinding.ActivityLoginBinding
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -21,13 +21,16 @@ class LoginActivity : AppCompatActivity() {
         binding.loginBtn.setOnClickListener {
             val email = binding.emailEdit.text.toString()
             val password = binding.passwordEdit.text.toString()
+            Log.e("LoginActivity", "email = ${email} password = ${password}")
 
             Firebase.auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) {
                     if(it.isSuccessful) {
+                        Log.e("LoginActivity", "로그인 성공")
                         startActivity(Intent(this, FeedActivity::class.java)) // FeedActivity로 이동
                         finish()
                     } else {
+                        Log.e("LoginActivity", "로그인 실패")
                         binding.loginWarning.text = "로그인 실패!"
                     }
                 }
